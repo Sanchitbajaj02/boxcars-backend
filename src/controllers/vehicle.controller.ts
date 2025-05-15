94476316bd7a1918c1ffc18074731977f3ef8698
Sanchit Bajaj <sanchit02@outlook.com>
2025-05-15 23:46:52 +0530

    creation of post and delete router

--- src/controllers/vehicle.controller.ts
+++ src/controllers/vehicle.controller.ts
@@ -29,3 +29,43 @@ export const getSingleVehicleHandler = catchErrors(async (req, res) => {
     message: "Get single vehicle",
   });
 });
+
+export const saveVehicleHandler = catchErrors(async (req, res) => {
+  const request = vehicleSchema.parse(req.body);
+
+  const vehicle = await VehicleModel.create({
+    vehicleMake: request.vehicleMake,
+    vehicleModel: request.vehicleModel,
+    vehicleYear: request.vehicleYear,
+    vehiclePrice: request.vehiclePrice,
+    vehicleMileage: request.vehicleMileage,
+    vehicleFuelType: request.vehicleFuelType,
+    vehicleTransmission: request.vehicleTransmission,
+  });
+
+  appAssert(vehicle, StatusCodes.BAD_REQUEST, "Error in saving vehicle");
+
+  return res.status(StatusCodes.CREATED).json({
+    message: "Vehicle created successfully",
+  });
+});
+
+export const deleteVehicleHandler = catchErrors(async (req, res) => {
+  const vehicleId = req.params.vehicleId;
+
+  const parsedVehicleId = vehicleIdSchema.parse(vehicleId);
+
+  const vehicleExist = await VehicleModel.exists({
+    _id: parsedVehicleId,
+  });
+
+  appAssert(vehicleExist, StatusCodes.FORBIDDEN, "Vehicle does not exist in the system");
+
+  await VehicleModel.deleteOne({
+    _id: parsedVehicleId,
+  });
+
+  return res.status(StatusCodes.OK).json({
+    message: "Vehicle deleted successfully",
+  });
+});
--- src/routes/vehicle.routes.ts
+++ src/routes/vehicle.routes.ts
@@ -1,16 +1,21 @@
 import { Router } from "express";
 const vehicleRouter = Router();
 
-import { getAllVehiclesHandler, getSingleVehicleHandler } from "@/controllers/vehicle.controller";
+import {
+  getAllVehiclesHandler,
+  getSingleVehicleHandler,
+  saveVehicleHandler,
+  deleteVehicleHandler,
+} from "@/controllers/vehicle.controller";
 
 vehicleRouter.get("/", getAllVehiclesHandler);
 
 vehicleRouter.get("/:vehicleId", getSingleVehicleHandler);
 
-vehicleRouter.post("/vehicle");
+vehicleRouter.post("/", saveVehicleHandler);
 
 // vehicleRouter.put("/vehicle");
 
-// vehicleRouter.delete("/vehicle");
+vehicleRouter.delete("/:vehicleId", deleteVehicleHandler);
 
 export default vehicleRouter;
